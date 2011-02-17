cd2ad56e3820f6695236f177df397efbbd638351
Mario Domenech Goulart <mario.goulart@gmail.com>
2011-02-17 23:33:10 +0000

    html-utils: SXML generation support. `itemize' and `enumerate' are still buggy for nested lists in SXML mode (is it possible to fix?)

--- html-utils.scm
+++ html-utils.scm
@@ -9,43 +9,109 @@
    submit-input)
 
 (import chicken scheme files data-structures posix utils)
-(use html-tags srfi-13)
+(use html-tags srfi-13 srfi-1)
+
+(define (list-attribs attribs)
+  (let ((attribs
+         (filter-map (lambda (attrib)
+                       (let ((value (cdr attrib)))
+                         (and value
+                              (list (car attrib) value))))
+                     attribs)))
+    (if (null? attribs)
+        '()
+        (list (cons '@ attribs)))))
+
+
+;;; tabularize
+(define (sxml-tabularize data #!key table-id table-class even-row-class odd-row-class header thead/tbody)
+  (let ((even-row #f))
+    (append '(table)
+            (list-attribs `((id . ,table-id)
+                            (class . ,table-class)))
+            (if header
+                (let ((h `(tr ,@(map (lambda (item) `(th ,item)) header))))
+                  (if thead/tbody
+                      `((thead ,h))
+                      `(,h)))
+                '())
+            (let ((body
+                   (map (lambda (line)
+                          (append '(tr)
+                                  (list-attribs `((class . ,(if even-row
+                                                                even-row-class
+                                                                odd-row-class))))
+                                  (begin
+                                    (set! even-row (not even-row))
+                                    (map (lambda (cell) `(td ,cell)) line))))
+                        data)))
+              (if thead/tbody
+                  `((tbody ,body))
+                  body)))))
+
 
 (define (tabularize data #!key table-id table-class quote-procedure even-row-class odd-row-class header thead/tbody)
-  (let ((even-row #f))
-    (<table> id: table-id class: table-class quote-procedure: quote-procedure
-             (string-append
-              (if header
-                  (let ((h (<tr> (string-intersperse (map <th> header) ""))))
+  (if (generate-sxml?)
+      (sxml-tabularize data
+                       table-id: table-id
+                       table-class: table-class
+                       even-row-class: even-row-class
+                       odd-row-class: odd-row-class
+                       header: header
+                       thead/tbody: thead/tbody)
+      (let ((even-row #f))
+        (<table> id: table-id class: table-class quote-procedure: quote-procedure
+                 (string-append
+                  (if header
+                      (let ((h (<tr> (string-intersperse (map <th> header) ""))))
+                        (if thead/tbody
+                            (<thead> h)
+                            h))
+                      "")
+                  (let ((body
+                         (string-intersperse
+                          (map (lambda (line)
+                                 (<tr> class: (and even-row-class odd-row-class
+                                                   (begin
+                                                     (set! even-row (not even-row))
+                                                     (if even-row
+                                                         even-row-class
+                                                         odd-row-class)))
+                                       (string-intersperse (map <td> line) "")))
+                               data)
+                          "")))
                     (if thead/tbody
-                        (<thead> h)
-                        h))
-                  "")
-              (let ((body
-                     (string-intersperse
-                      (map (lambda (line)
-                             (<tr> class: (and even-row-class odd-row-class
-                                               (begin
-                                                 (set! even-row (not even-row))
-                                                 (if even-row
-                                                     even-row-class
-                                                     odd-row-class)))
-                                   (string-intersperse (map <td> line) "")))
-                           data)
-                      "")))
-                (if thead/tbody
-                    (<tbody> body)
-                    body))))))
+                        (<tbody> body)
+                        body)))))))
+
+
+;;; itemize & enumerate
+(define (sxml-list listing self items #!key list-id list-class)
+  (cons listing
+        (append
+         (list-attribs `((id . ,list-id)
+                         (class . ,list-class)))
+         (map (lambda (item)
+                (if (list? item)
+                    (self item)
+                    `(li ,item)))
+              items))))
 
 (define (html-list listing self items #!key list-id list-class quote-procedure)
-  (listing id: list-id class: list-class quote-procedure: quote-procedure
-           (string-intersperse
-            (map (lambda (item)
-                   (if (list? item)
-                       (self item quote-procedure: quote-procedure)
-                       (<li> item)))
-                 items)
-            "")))
+  (if (generate-sxml?)
+      (sxml-list 'ul
+                 itemize
+                 items
+                 list-id: list-id
+                 list-class: list-class)
+      (listing id: list-id class: list-class quote-procedure: quote-procedure
+               (string-intersperse
+                (map (lambda (item)
+                       (if (list? item)
+                           (self item quote-procedure: quote-procedure)
+                           (<li> item)))
+                     items)
+                ""))))
 
 (define (itemize items #!key list-id list-class quote-procedure)
   (html-list <ul>
@@ -63,56 +129,130 @@
              list-class: list-class
              quote-procedure: quote-procedure))
 
-(define (html-page contents #!key css title (doctype "") (headers "") charset)
-  (string-append
-   doctype
-   (<html>
-    (<head>
-     (if title (<title> title) "")
-     (if charset
-         (<meta> http-equiv: "Content-Type"
-                 content:  (string-append "text/html; charset=" charset))
-         "")
-     (cond ((string? css)
-            (<link> rel: "stylesheet" href: css type: "text/css"))
-           ((list? css)
-            (let ((inline ""))
-              (string-intersperse
-               (map (lambda (f)
-                      (if (list? f)
-                          (<style> (read-all (make-pathname (current-directory) (car f))))
-                          (<link> rel: "stylesheet" href: f type: "text/css")))
-                    css)
-               "")))
-           (else ""))
-     headers)
-    (if (string-prefix-ci? "<body" contents)
-        contents
-        (<body> contents)))))
 
+;;; html-page
+(define (sxml-page contents #!key css title doctype headers charset)
+  (let ((page
+         `(html
+           ,(append '(head)
+                    (if title `((title ,title)) '())
+                    (if charset
+                        `((meta (@ (http-equiv "Content-Type")
+                                   (content ,(string-append "text/html; charset=" charset)))))
+                        '())
+                    (cond ((string? css)
+                           `((link (@ (rel "stylesheet")
+                                      (href ,css)
+                                      (type "text/css")))))
+                          ((list? css)
+                           (map (lambda (f)
+                                  (if (list? f)
+                                      `(style ,(read-all (make-pathname (current-directory) (car f))))
+                                      `(link (@ (rel "stylesheet")
+                                                (href ,f)
+                                                (type "text/css")))))
+                                css))
+                          (else '()))
+                    (if headers `(,headers) '()))
+           ,(if (string? contents)
+                `(body ,contents)
+                `(body ,@contents)))))
+    (if doctype
+        (append `((literal ,doctype)) `(,page))
+        page)))
+
+(define (html-page contents #!key css title doctype headers charset)
+  (if (generate-sxml?)
+      (sxml-page contents
+                 css: css
+                 title: title
+                 doctype: doctype
+                 headers: headers
+                 charset: charset)
+      (string-append
+       (or doctype "")
+       (<html>
+        (<head>
+         (if title (<title> title) "")
+         (if charset
+             (<meta> http-equiv: "Content-Type"
+                     content:  (string-append "text/html; charset=" charset))
+             "")
+         (cond ((string? css)
+                (<link> rel: "stylesheet" href: css type: "text/css"))
+               ((list? css)
+                (string-intersperse
+                 (map (lambda (f)
+                        (if (list? f)
+                            (<style> (read-all (make-pathname (current-directory) (car f))))
+                            (<link> rel: "stylesheet" href: f type: "text/css")))
+                      css)
+                 ""))
+               (else ""))
+         (or headers ""))
+        (if (string-prefix-ci? "<body" contents)
+            contents
+            (<body> contents))))))
+
+
+;;; combo-box
+(define (sxml-make-options options #!optional default first-empty)
+  (let ((opts (map (lambda (opt)
+                     (let ((val (->string (cond ((pair? opt) (car opt))
+                                                ((vector? opt) (vector-ref opt 0))
+                                                (else opt))))
+                           (text (->string (cond ((list? opt) (cadr opt))
+                                                 ((pair? opt) (cdr opt))
+                                                 ((vector? opt) (vector-ref opt 1))
+                                                 (else opt)))))
+                       (<option> value: val
+                                 selected: (and default (equal? val (->string default)))
+                                 text)))
+                   options)))
+    (if first-empty
+        (cons (<option> selected: (and default (equal? "" (->string default))))
+              opts)
+        opts)))
 
 (define (make-options options #!optional default first-empty)
-  (string-append
-   (if first-empty
-       (<option> selected: (and default (equal? "" (->string default))))
-       "")
-   (string-intersperse
-    (map (lambda (opt)
-           (let ((val (->string (cond ((pair? opt) (car opt))
-                                      ((vector? opt) (vector-ref opt 0))
-                                      (else opt))))
-                 (text (->string (cond ((list? opt) (cadr opt))
-                                       ((pair? opt) (cdr opt))
-                                       ((vector? opt) (vector-ref opt 1))
-                                       (else opt)))))
-             (<option> value: val
-                       selected: (and default (equal? val (->string default)))
-                       text)))
-         options)
-    "")))
+  (if (generate-sxml?)
+      (sxml-make-options options default first-empty)
+      (string-append
+       (if first-empty
+           (<option> selected: (and default (equal? "" (->string default))))
+           "")
+       (string-intersperse
+        (map (lambda (opt)
+               (let ((val (->string (cond ((pair? opt) (car opt))
+                                          ((vector? opt) (vector-ref opt 0))
+                                          (else opt))))
+                     (text (->string (cond ((list? opt) (cadr opt))
+                                           ((pair? opt) (cdr opt))
+                                           ((vector? opt) (vector-ref opt 1))
+                                           (else opt)))))
+                 (<option> value: val
+                           selected: (and default (equal? val (->string default)))
+                           text)))
+             options)
+        ""))))
 
 (define (combo-box name options #!key default id first-empty onchange onkeyup disabled
                    length multiple selectedindex size tabindex type class)
+  (if (generate-sxml?)
+      (append '(select)
+              (list-attribs `((name . ,name)
+                              (id . ,(or id name))
+                              (onchange . ,onchange)
+                              (onkeyup . ,onkeyup)
+                              (disabled . ,disabled)
+                              (length . ,length)
+                              (multiple . ,multiple)
+                              (selectedindex . ,selectedindex)
+                              (size . ,size)
+                              (tabindex . ,tabindex)
+                              (type . ,type)
+                              (class . ,class)))
+              (sxml-make-options options default first-empty))
   (<select> onchange: onchange
             onkeyup: onkeyup
             disabled: disabled
@@ -125,20 +265,35 @@
             name: name
             id: (or id name)
             class: class
-            (make-options options default first-empty)))
+            (make-options options default first-empty))))
+
+
+;;; inputs
+(define (sxml-hidden-input name #!optional value id)
+  (if (list? name)
+      (map (lambda (item)
+             (let ((name (->string (car item))))
+               (<input> type: "hidden"
+                        id: (or id name)
+                        name: name
+                        value: (->string (cdr item)))))
+           name)
+      (<input> type: "hidden" name: name id: (or id name) value: value)))
 
 (define (hidden-input name #!optional value id)
-  (if (list? name)
-      (string-intersperse
-       (map (lambda (item)
-              (let ((name (->string (car item))))
-                (<input> type: "hidden"
-                         id: (or id name)
-                         name: name
-                         value: (->string (cdr item)))))
-            name)
-       "")
-      (<input> type: "hidden" name: name id: (or id name) value: value)))
+  (if (generate-sxml?)
+      (sxml-hidden-input name value id)
+      (if (list? name)
+          (string-intersperse
+           (map (lambda (item)
+                  (let ((name (->string (car item))))
+                    (<input> type: "hidden"
+                             id: (or id name)
+                             name: name
+                             value: (->string (cdr item)))))
+                name)
+           "")
+          (<input> type: "hidden" name: name id: (or id name) value: value))))
 
 (define (text-input name . args)
   (apply <input>
--- tests/run.scm
+++ tests/run.scm
@@ -117,6 +117,163 @@
         (delete-file "style2.css")
         page))
 
+;;; SXML
+(generate-sxml? #t)
+
+;;; tabularize
+(test '(table (tr (td "a"))) (tabularize '(("a"))))
+
+(test '(table (tr (td 1) (td 2) (td 3)) (tr (td 4) (td 5) (td 6)))  (tabularize '((1 2 3) (4 5 6))))
+
+(test '(table (@ (id "test")) (tr (td 1) (td 2) (td 3)) (tr (td 4) (td 5) (td 6)))
+      (tabularize '((1 2 3) (4 5 6)) table-id: "test"))
+
+(test '(table (@ (id "test")) (tr (@ (class "blue")) (td 1) (td 2) (td 3)) (tr (@ (class "yellow")) (td 4) (td 5) (td 6)))
+      (tabularize '((1 2 3) (4 5 6)) table-id: "test" even-row-class: "yellow" odd-row-class: "blue"))
+
+(test '(table (tr (th a) (th b) (th c)) (tr (td 1) (td 2) (td 3)) (tr (td 4) (td 5) (td 6)))
+      (tabularize '((1 2 3) (4 5 6)) header: '(a b c)))
+
+;;; itemize & enumerate
+(test '(ul (li 1) (li 2)) (itemize '(1 2)))
+
+(test '(ul (@ (id "foo")) (li 1) (li 2)) (itemize '(1 2) list-id: "foo"))
+
+(test '(ul (li 1) (li 2) (ul (li 3) (li 4))) (itemize '(1 2 (3 4))))
+
+;; from awful
+(test '(ul (@ (id "button-bar"))
+           (li (button (@ (id "eval")) "Eval"))
+           (li (button (@ (id "eval-region")) "Eval region"))
+           (li (button (@ (id "clear")) "Clear")))
+      (itemize
+       (map (lambda (item)
+              (<button> id: (car item) (cdr item)))
+            (append '(("eval"  . "Eval"))
+                    '(("eval-region" . "Eval region"))
+                    '(("clear" . "Clear"))))
+       list-id: "button-bar"))
+
+;;; html-page
+(test '(html (head) (body)) (html-page '()))
+(test '(html (head (title "title")) (body "foo")) (html-page "foo" title: "title"))
+
+(test '((lit "<test-doctype>") (html (head) (body)))
+      (html-page '() doctype: "<test-doctype>"))
+
+(test '(html (head (title "title")) (body))
+      (html-page '() title: "title"))
+
+(test '(html (head (title "title") (meta (@ (http-equiv "Content-Type") (content "text/html; charset=UTF-8")))) (body))
+      (html-page '() title: "title" charset: "UTF-8"))
+
+(test '(html (head (title "title") (script (@ (type "text/javascript") (src "js.js")))) (body))
+      (html-page '()
+                 title: "title"
+                 headers: (<script> type: "text/javascript" src: "js.js")))
+
+(test '(html
+        (head (title "title")
+              (link (@ (rel "stylesheet") (href "style.css") (type "text/css")))
+              (script (@ (type "text/javascript") (src "js.js"))))
+        (body))
+      (html-page '()
+                 title: "title"
+                 headers: (<script> type: "text/javascript" src: "js.js")
+                 css: "style.css"))
+
+(test '(html (head (title "title")
+                   (link (@ (rel "stylesheet") (href "style.css") (type "text/css")))
+                   (script (@ (type "text/javascript") (src "js.js"))))
+             (body))
+      (html-page '()
+                 title: "title"
+                 headers: (<script> type: "text/javascript" src: "js.js")
+                 css: '("style.css")))
+
+(test '(html (head (title "title")
+                   (link (@ (rel "stylesheet") (href "style.css") (type "text/css")))
+                   (link (@ (rel "stylesheet") (href "style2.css") (type "text/css")))
+                   (script (@ (type "text/javascript") (src "js.js"))))
+             (body))
+      (html-page '()
+                 title: "title"
+                 headers: (<script> type: "text/javascript" src: "js.js")
+                 css: '("style.css" "style2.css")))
+
+(test '(html (head (title "title")
+                   (link (@ (rel "stylesheet") (href "style.css") (type "text/css")))
+                   (style "body { font-size: 10pt; }\n")
+                   (script (@ (type "text/javascript") (src "js.js"))))
+             (body))
+      (let ((page #f))
+        (with-output-to-file "style2.css" (cut print "body { font-size: 10pt; }"))
+        (set! page
+              (html-page '()
+                         title: "title"
+                         headers: (<script> type: "text/javascript" src: "js.js")
+                         css: '("style.css" ("style2.css"))))
+        (delete-file "style2.css")
+        page))
+
+;;; combo-box
+(test '(select (@ (name "test") (id "test"))
+               (option (@ (value "1")) "1")
+               (option (@ (value "2")) "2")
+               (option (@ (value "3")) "3"))
+      (combo-box "test" '(1 2 3)))
+
+(test '(select (@ (name "test") (id "test"))
+               (option (@ (value "1")) "1")
+               (option (@ (value "2")) "2")
+               (option (@ (value "3")) "3"))
+      (combo-box "test" '((1 . 1) (2 . 2) (3 . 3))))
+
+(test '(select (@ (name "test") (id "test"))
+               (option (@ (value "1")) "1")
+               (option (@ (value "2")) "2")
+               (option (@ (value "3")) "3"))
+      (combo-box "test" '(#(1 1) #(2 2) #(3 3))))
+
+(test '(select (@ (name "test") (id "test"))
+               (option (@ (value "1") (selected)) "1")
+               (option (@ (value "2")) "2")
+               (option (@ (value "3")) "3"))
+      (combo-box "test" '(#(1 1) #(2 2) #(3 3)) default: "1"))
+
+(test '(select (@ (name "test") (id "test"))
+               (option)
+               (option (@ (value "1")) "1")
+               (option (@ (value "2")) "2")
+               (option (@ (value "3")) "3"))
+      (combo-box "test" '(#(1 1) #(2 2) #(3 3)) first-empty: #t))
+
+(test '(select (@ (name "test") (id "test"))
+               (option)
+               (option (@ (value "1")) "1")
+               (option (@ (value "2") (selected)) "2")
+               (option (@ (value "3")) "3"))
+      (combo-box "test" '(#(1 1) #(2 2) #(3 3)) first-empty: #t default: 2))
+
+(test '(select (@ (name "test") (id "test"))
+               (option (@ (selected)))
+               (option (@ (value "1")) "1")
+               (option (@ (value "2")) "2")
+               (option (@ (value "3")) "3"))
+      (combo-box "test" '(#(1 1) #(2 2) #(3 3)) first-empty: #t default: ""))
+
+;;; hidden-input
+
+(test '(input (@ (type "hidden") (name "test") (id "test")))
+      (hidden-input "test"))
+
+(test '(input (@ (type "hidden") (name "test") (id "test") (value "0")))
+      (hidden-input "test" 0))
+
+(test '((input (@ (type "hidden") (id "test") (name "test") (value "0")))
+        (input (@ (type "hidden") (id "test2") (name "test2") (value "1"))))
+      (hidden-input '((test . 0) (test2 . 1))))
+
 (test-end "html-utils")
 
-(unless (zero? (test-failure-count)) (exit 1))
+(test-exit)
